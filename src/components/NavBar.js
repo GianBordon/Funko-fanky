baeee540363ed151414502d9a885cfb66f20b25c
Gianfranco Bordon <shiammdp21@gmail.com>
2023-03-23 19:31:57 -0300

    arreglos

---
+++ src/components/NavBar.js
@@ -0,0 +1,20 @@
+import styles from "./NavBar/NavBar.module.css";
+import CartWidget from "./CartWidget/CartWidget";
+
+
+const NavBar = () => {
+    return (
+        <nav>
+            <h3>Funko Funky</h3>
+            <div>
+                <button>Star Wars</button>
+                <button>Marvel</button>
+                <button>DC</button>
+                <button>Disney</button>
+            </div>
+            <CartWidget/>
+        </nav>
+    )
+}
+
+export default NavBar;
