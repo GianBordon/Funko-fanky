1a3d24b2c594718877d23ff9cd8d44d9df051b23
Gianfranco Bordon <shiammdp21@gmail.com>
2023-04-12 15:23:21 -0300

    Arreglos 2da Pre-Entrega

--- src/components/asyncMock.js
+++ src/components/asyncMock.js
@@ -1,3 +1,11 @@
 const productos = [
     {id:'1', name:'',price:'',category:'',img:'',stock:'20',description:''}
 ]
+
+export const getProductos = () => {
+    return new Promise ((resolve) => {
+        setTimeout(() => {
+            resolve(productos)
+        }, 500)
+    })
+}
