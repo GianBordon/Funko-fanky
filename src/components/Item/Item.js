2226619f9efb5d414911ea7d248e617741216856
Gianfranco Bordon <shiammdp21@gmail.com>
2023-04-13 17:50:11 -0300

    Arreglos Finales

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -1,5 +1,6 @@
 import './Item.css'
-
+import {Link} from 'react-router-dom'
+ 
 const Item = ({id, name, img, price, stock}) => {
 
     return(
@@ -21,7 +22,7 @@ const Item = ({id, name, img, price, stock}) => {
                 </p>
             </section>
             <footer className='ItemFooter'>
-                <link to={`/item/${id}`} className='Option'>Ver detalle</link>
+                <Link to={`/item/${id}`} className='Option'>Ver detalle</Link>
             </footer>
         </article>
     )
--- src/components/ItemDetailContainer/ItemDetailContainer.js
+++ src/components/ItemDetailContainer/ItemDetailContainer.js
@@ -6,7 +6,7 @@ import { useParams } from 'react-router-dom'
 
 const ItemDetailContainer = () => {
     const [productos, setProductos] = useState(null)
-    const {itemId} = useParams
+    const {itemId} = useParams ()
     useEffect(() =>{
         getProductosById(itemId)
             .then(response =>{
