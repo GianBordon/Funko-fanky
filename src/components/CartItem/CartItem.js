84d0708a58599f99a3b11eaf63e73fffc28f4a49
Gianfranco Bordon <shiammdp21@gmail.com>
2023-05-04 12:52:39 -0300

    corrito realizado

--- src/components/CartItem/CartItem.js
+++ src/components/CartItem/CartItem.js
@@ -1,11 +1,17 @@
+import { useContext } from 'react';
+import { CartContext } from '../../Context/CartContext';
 import './CartItem.css'
 
 
-const CartItem = ({productos}) => {
-console.log(productos)
+const CartItem = ({name, price, quantity, id,img}) => {
+const {removeItem} = useContext(CartContext)
 return(
-<div>
-<h1>Cart Item </h1>
+<div className='carrito'>
+<h1>{img}</h1>
+<h1>{name} </h1>
+<h1>{price} </h1>
+<h1>cantidad:{quantity} </h1>
+<button onClick={()=>removeItem(id)} className='Eliminar'>X</button>
 </div>
 
 )
